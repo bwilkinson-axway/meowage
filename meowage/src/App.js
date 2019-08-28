baa8cdda8145342febdca29fc3175cff84a43e64
bradtxufl <brad.w@utexas.edu>
2019-08-27 17:01:02 -0700

    login page

--- meowage/src/App.js
+++ meowage/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <img class="pic" src="jRFox.jpg"/>
+      <div class="placemat">
+      <div class="card">
+      <form>
+      <h3>Meowage: You've only got 9 lives...make them count!</h3>
+
+      <label>Email Address</label>
+      <br></br>
+      <input type="email" name="email"></input>
+      <br></br>
+      <label>Password</label>
+      <br></br>
+      <input type="password" name="password"></input>
+      <br></br>
+      <button type="submit" name="submit">Sign In</button>
+      </form>
+      <button>I need to make an account</button>
+      </div>
+      </div>
     </div>
   );
 }
