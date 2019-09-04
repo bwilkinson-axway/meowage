aa0baec515d8a3d827e4d972594acff4c196efb6
bradtxufl <brad.w@utexas.edu>
2019-09-04 14:52:32 -0700

    updated readme

--- meowage/src/Profile.js
+++ meowage/src/Profile.js
@@ -18,6 +18,7 @@ class Profile extends React.Component {
           <h5> Age: {this.props.state.cats[this.props.match.params.id].age} </h5>
           <h5> Favorite Toy: {this.props.state.cats[this.props.match.params.id].favorite_toy} </h5>
           <button onClick={() => this.handleClick()}><img id='matches' src={this.props.state.meow.includes(parseInt(this.props.match.params.id)) ? 'https://clipart.info/images/ccovers/1495818555cartoon-fire-flames-emoji-png-transparent.png' : "https://www.actaturcica.com/wp-content/uploads/2018/06/Match-Stick-PNG-Transparent-Image.png"} alt='matches'></img></button>
+          <h4> {this.props.state.meow.includes(parseInt(this.props.match.params.id)) ? "Email: " + this.props.state.cats[this.props.match.params.id].email : ""} </h4>
         </div>
       </div>
     );
